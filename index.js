41a19042d7c4e38da25c8079815d9b90f972cd97
JFAquinoPando <pandolcik@gmail.com>
2025-02-12 00:34:02 -0300

    prueba

---
+++ index.js
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+(async () => {
+  try {
+    const user = await prisma.usuario.create({
+      data: {
+        email: 'fabricio@idt.com.py',
+        name: 'Fabricio',
+      },
+    });
+    console.log('Usuario creado:', user);
+
+    const users = await prisma.usuario.findMany();
+    console.log('Usuarios en la BD:', users);
+
+    const updatedUser = await prisma.usuario.update({
+      where: { id: user.id },
+      data: { name: 'José Fabricio', nickname: "JFAquinoPando" },
+    });
+    console.log('Usuario actualizado:', updatedUser);
+
+    /* await prisma.usuario.delete({ where: { id: user.id } });
+    console.log('Usuario eliminado'); */
+  } catch (error) {
+    console.error(error); // en caso de error mostrarmos 
+  } finally {
+    await prisma.$disconnect();
+  }
+})();
+
